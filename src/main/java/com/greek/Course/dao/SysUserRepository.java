42bd1e7a8ad51fcf2e8d5100225854f094c74ebc
Zhaofeng Zhou <zhaofengzhou9@gmail.com>
2022-06-10 17:23:04 +0800

    添加用户增删改查接口

--- src/main/java/com/greek/Course/dao/SysUserRepository.java
+++ src/main/java/com/greek/Course/dao/SysUserRepository.java
@@ -1,13 +1,21 @@
 package com.greek.Course.dao;
 
 import com.greek.Course.model.SysUser;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * @author Zhaofeng Zhou
  * @since 2022/5/30
  */
-public interface SysUserRepository extends CrudRepository<SysUser, Integer> {
+public interface SysUserRepository extends JpaRepository<SysUser, Integer> {
 
     SysUser findByUsername(String username);
+
+    @Query(value = "select u from SysUser u where u.username like %:search%")
+    Page<SysUser> findBySearch(@Param("search") String search, Pageable pageable);
 }
